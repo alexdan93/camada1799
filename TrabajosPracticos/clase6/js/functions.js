839c63c663f6507e63f176f23fbd35b7c8059221
Alex <alexdan93@hotmail.com>
2017-04-09 20:37:16 -0300

    Agregado de diseño responsive tp clase 6

---
+++ TrabajosPracticos/clase6/js/functions.js
@@ -0,0 +1,26 @@
+// function adjustStyle(width) {
+// 	width = parseInt(width);
+// 	if (width < 701) {
+// 		$("#size-stylesheet").attr("href", "css/narrow.css");
+// 	} else if (width < 900) {
+// 		$("#size-stylesheet").attr("href", "css/medium.css");
+// 	} else {
+// 		$("#size-stylesheet").attr("href", "css/wide.css"); 
+// 	}
+// }
+
+// $(function() {
+// 	adjustStyle($(this).width());
+// 	$(window).resize(function() {
+// 		adjustStyle($(this).width());
+// 	});
+// });
+
+// $(function() {
+// 	var menu = $('.menu');
+// 	var width = $(this).width();
+// 	if(width < 601){
+// 		menu;
+// 	}
+// });
+
